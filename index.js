ca130cbbb15d9334f70198a2e54c72f67c3c9880
Hom Dahal <homdahal997@gmail.com>
2024-04-23 17:07:11 -0400

    part 1 completed

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import "./styles.css";
-
 // Part 1: Getting Started
 // Select and cache the <main> element in a variable named mainEl.
+const mainEl = document.querySelector("main");
+// Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+mainEl.style.background = "var(--main-bg)";
+// Set the content of mainEl to <h1>DOM Manipulation</h1>. 
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+// Add a class of flex-ctr to mainEl.
+mainEl.classList.add("flex-ctr");
+
+
